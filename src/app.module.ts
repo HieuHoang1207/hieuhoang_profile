39066b5be3550a67793b73652b69f7fd391cd498
Hieu.mh7 <hieu.mh7@gmail.com>
2025-02-16 21:54:59 +0700

    them page test

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { GifController } from './gifcontroller'; // Đổi tên controller ở đây
+import { SodaController } from './sodacontroller';
 import { AppService } from './app.service';
 
 @Module({
   imports: [],
-  controllers: [AppController, GifController], // Đảm bảo tên controller ở đây đúng
+  controllers: [AppController, GifController, SodaController], // Đảm bảo tên controller ở đây đúng
   providers: [AppService],
 })
 export class AppModule {}
